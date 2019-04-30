0d50316ebd8c5fdafbdee0b36077515e46552f0d
Paul Brickles <paul.brickles@ii.co.uk>
2019-04-30 22:10:17 +0100

    🐛 Fix build bugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Hi Henry!</h1>
   </Layout>
 )
 
