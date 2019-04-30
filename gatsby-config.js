e443db8e99f83aeee4248cbc070104b0b94f2419
Paul Brickles <paul.brickles@ii.co.uk>
2019-04-30 21:46:29 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  siteMetadata: {
+    title: `52 Skidoo`,
+    description: `52 Skidoo`,
+    author: `@pbrickles`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-cms`,
+  ],
+}
